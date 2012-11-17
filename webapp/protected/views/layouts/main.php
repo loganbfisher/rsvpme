788e0e7bf4332a17b7c1142857d6419ceba89ced
Logan Fisher <lfisher@lfisher.allplayers.local.home>
2012-11-16 23:06:55 -0600

    added files to git

---
+++ webapp/protected/views/layouts/main.php
@@ -0,0 +1,52 @@
+<?php /* @var $this Controller */ ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="language" content="en" />
+
+    <!-- blueprint CSS framework -->
+    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
+    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
+    <!--[if lt IE 8]>
+    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
+    <![endif]-->
+
+    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
+    <link href='http://fonts.googleapis.com/css?family=Patua+One' rel='stylesheet' type='text/css'>
+      <script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/jquery-1.7.2.min.js"></script>
+      <title><?php echo CHtml::encode($this->pageTitle); ?></title>
+  </head>
+
+  <body>
+    <script>
+      $(document).ready(function(){
+
+        $('.login-wrapper').hide();
+        $('#mainmenu').hide();
+        $('.login-text').click(function(){
+          $('.login-wrapper').slideToggle();
+        });
+
+        var defaultText = $('input').attr('value');
+        $('input').focus(function(){
+          $(this).attr('value','');
+        });
+
+      });
+    </script>
+    <div class="login-wrapper">
+      <div class="login-container">
+        <?php echo $this->renderPartial('/site/login/_login', array('model' => new LoginForm())); ?>
+      </div>
+      <div class="clear"></div>
+    </div>
+    <div class="header_wrapper"><?php echo $this->renderPartial('/site/global/_header'); ?></div>
+    <div class="container" id="page">
+      <?php echo $content; ?>
+      <div class="clear"></div>
+      <?php echo $this->renderPartial('/site/global/_footer'); ?>
+    </div><!-- page -->
+  </body>
+</html>
---
+++ webapp/protected/views/site/global/_header.php
@@ -0,0 +1,7 @@
+<div class="header">
+  <h1 class="logo">
+    <?= CHtml::link('rsvp.me', array('/site/index')); ?>
+  </h1>
+  <?= CHtml::link('Sign in', '#', array('class' => 'login-text fright')); ?>
+  <div class="clear"></div>
+</div>
